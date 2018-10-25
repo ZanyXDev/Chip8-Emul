0a1a25951bb568a07f6b81d4c19bdc46c1b5c99d
zanyxdev <zanyxdev@gmail.com>
2018-10-25 14:51:09 +0300

    make load data to virtual (emulated) memory

--- src/chip8emu.cpp
+++ src/chip8emu.cpp
@@ -1,6 +1,21 @@
+#include <QDebug>
 #include "chip8emu.h"
 
-Chip8Emu::Chip8Emu(QObject *parent) : QObject(parent)
+Chip8Emu::Chip8Emu(QObject *parent)
+    : QObject(parent)
+    , PC(START_ADDR)
 {
+    m_memory.fill(0x0,RAM_SIZE);   // clear 4k ram memory
+}
+
+void Chip8Emu::loadData2Memory(QByteArray &data)
+{
+    qDebug() << "load: " << data.size() << " bytes";
+    if ( !data.isEmpty() &&
+         ( data.size() <= RAM_SIZE - START_ADDR)  ) {
+        m_memory.insert(START_ADDR,data);
+
+        emit ReadyToWork(true);
+    }
 
 }
--- src/chip8emu.h
+++ src/chip8emu.h
@@ -1,7 +1,12 @@
 #ifndef CHIP8EMU_H
 #define CHIP8EMU_H
 
+#include <QByteArray>
 #include <QObject>
+
+#define RAM_SIZE 4096
+#define START_ADDR 0x200
+
 
 class Chip8Emu : public QObject
 {
@@ -10,8 +15,14 @@ public:
     explicit Chip8Emu(QObject *parent = nullptr);
 
 signals:
+    void ReadyToWork(bool);
 
 public slots:
+    void loadData2Memory(QByteArray &data);
+private:
+    unsigned short PC;   // mem offset counter
+    QByteArray m_memory; // 4k ram memory
+
 };
 
 #endif // CHIP8EMU_H
