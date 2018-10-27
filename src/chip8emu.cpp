fa9eda51f1599db062f5fbe6406ecb24e7b6f8d2
zanyxdev <zanyxdev@gmail.com>
2018-10-27 12:54:35 +0300

    draw sprite function

--- src/chip8emu.cpp
+++ src/chip8emu.cpp
@@ -1,11 +1,19 @@
-#include <QDebug>
-#include "chip8emu.h"
+﻿#include "chip8emu.h"
 
 Chip8Emu::Chip8Emu(QObject *parent)
     : QObject(parent)
     , PC(START_ADDR)
 {
+    PC = START_ADDR;
+    regI = START_ADDR;
     m_memory.fill(0x0,RAM_SIZE);   // clear 4k ram memory
+    m_stopped = false;
+    opcode_count = 0 ;
+
+    delay_timer = 0;               // clear delay timer;
+    sound_timer = 0;               // clear sound timer;
+    m_screen = QBitArray(DISPLAY_X * DISPLAY_Y,false);  // clear screen map
+
 }
 
 void Chip8Emu::loadData2Memory(QByteArray &data)
@@ -16,6 +24,135 @@ void Chip8Emu::loadData2Memory(QByteArray &data)
         m_memory.insert(START_ADDR,data);
 
         emit ReadyToWork(true);
+        m_stopped = false;
+    }
+}
+
+void Chip8Emu::startEmulation()
+{
+
+    QElapsedTimer et;
+    et.start();
+
+    opcode_count = 0;
+    int cycles_per_second;
+
+    while (!m_stopped) {
+        QCoreApplication::processEvents ( QEventLoop::AllEvents );
+
+        if (m_mode == Chip8Emu::SuperMode) {
+            cycles_per_second = 10; // execute 600 opcodes per second
+        } else {
+            cycles_per_second = 30; // 1800 opcodes per second
+        }
+        if (opcode_count < cycles_per_second) {
+            executeNextOpcode();
+            opcode_count++;
+        }
+
+        //decrease timers every 1/60sec and redraw screen
+        if (et.hasExpired(1000/60)) {
+            decreaseTimers();
+            et.restart();
+            emit updateScreen(m_screen);
+            opcode_count = 0;
+        }
+        //if (emu->stop == true) closeRom();
     }
 
+}
+
+void Chip8Emu::stopEmulation()
+{
+    m_stopped = true;
+}
+
+void Chip8Emu::executeNextOpcode()
+{
+    //debugShowTime();
+    drawSprite(5,5,9);
+}
+
+void Chip8Emu::decreaseTimers()
+{
+    if (delay_timer > 0) {
+        --delay_timer;
+    }
+}
+
+void Chip8Emu::debugShowTime()
+{
+    // int idx = DISPLAY_X * DISPLAY_Y;
+    // for (int i=0; i< 24; ++i){
+    //        m_screen.toggleBit(QRandomGenerator::global()->bounded( idx ));
+    //  }
+
+}
+
+/**
+ * @brief drawSprite
+ * @param idx
+ * @param sprite
+ * @return
+ * @note реализует поведение опкода DXYN. Она рисует в массив m_screen
+ * спрайт находящийся по смещению I. Координаты спрайта берутся из регистров VX и VY.
+ * Один байт по смещению I представляет одну строку спрайта для CHIP-8,
+ * и полстроки для SCHIP (при N=0). Каждый бит в этом байте представляет отдельный пиксель.
+ * Количество строк N извлекается из опкода. Если N=0, то в спрайте 16 строк.
+ * Также не стоит забывать о том, что если мы рисуем пиксель “поверх” уже существующего
+ * пикселя, то эта точка экрана очистится, а регистр VF установится в 1.
+ * То есть рисуем методом XOR.
+ */
+bool Chip8Emu::drawSprite(int vx,int vy, int n)
+{
+    bool vf_flag = false;
+    bool showPixel;
+    unsigned short maxLine;
+    unsigned short drw;
+    unsigned short idx ;
+#ifdef DEBUG
+/**
+  * Test sprite 8x9 pixel
+  * DB $.111.... 0x70
+  * DB $1...1... 0x88
+  * DB $....1... 0x8
+  * DB $....1... 0x8
+  * DB $...1.... 0x10
+  * DB $..1..... 0x20
+  * DB $.1...... 0x40
+  * DB $1....... 0x80
+  * DB $11111... 0xf8
+*/
+    m_memory[regI] = 0x70;
+    m_memory[regI+1] = 0x88;
+    m_memory[regI+2] = 0x8;
+    m_memory[regI+3] = 0x8;
+    m_memory[regI+4] = 0x10;
+    m_memory[regI+5] = 0x20;
+    m_memory[regI+6] = 0x40;
+    m_memory[regI+7] = 0x80;
+    m_memory[regI+8] = 0xf8;
+#endif
+
+    if ( 0 == n){ // check how many rows draw
+        maxLine = 16;
+    }else {
+        maxLine = n;
+    }
+
+    for (int row = 0; row < maxLine; ++row){
+        drw = m_memory.at(regI+row);
+        for (int col = 0; col < 8; ++col){
+            showPixel = drw  & (1 << (7 - col));
+            idx = (vx + col) + ((vy + row) * DISPLAY_X);
+
+            if ( m_screen.testBit( idx ) && !showPixel){
+                vf_flag = true;
+            }
+
+            m_screen.setBit( idx, showPixel);
+        }
+    }
+
+    return vf_flag;
 }
--- src/chip8emu.h
+++ src/chip8emu.h
@@ -3,26 +3,58 @@
 
 #include <QByteArray>
 #include <QObject>
+#include <QDebug>
+#include <QElapsedTimer>
+#include <QCoreApplication>
+#include <QBitArray>
+#include <QRandomGenerator>
 
 #define RAM_SIZE 4096
 #define START_ADDR 0x200
+#define DISPLAY_X 64
+#define DISPLAY_Y 32
 
-
+#define DEBUG 1
 class Chip8Emu : public QObject
 {
     Q_OBJECT
 public:
+    enum WorkModeFlag {
+        Emulation = 0x1,
+        Debuging  = 0x2,
+        SuperMode = 0x5,
+        SuperModeDebug =  Debuging | SuperMode,
+        UnDefined = 0xf
+    };
+    Q_DECLARE_FLAGS(WorkMode, WorkModeFlag)
+
     explicit Chip8Emu(QObject *parent = nullptr);
 
 signals:
-    void ReadyToWork(bool);
+    void ReadyToWork( bool flag);
+    void updateScreen( QBitArray screen );
 
 public slots:
     void loadData2Memory(QByteArray &data);
+    void startEmulation();
+    void stopEmulation();
 private:
-    unsigned short PC;   // mem offset counter
-    QByteArray m_memory; // 4k ram memory
 
+    unsigned short PC;   // mem offset counter
+    unsigned short regI; // address register I
+    QByteArray m_memory; // 4k ram memory
+    QBitArray m_screen;
+
+    int opcode_count;
+    bool m_stopped;
+    WorkModeFlag m_mode;
+    short delay_timer;         // delay timer;
+    short sound_timer;         // sound timer;
+
+    bool drawSprite(int vx,int vy, int n);
+    void executeNextOpcode();
+    void decreaseTimers();
+    void debugShowTime();
 };
 
 #endif // CHIP8EMU_H
