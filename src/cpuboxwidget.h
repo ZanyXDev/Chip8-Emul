cc82d2947687f8b465baf9aa3e129add65e8f463
zanyxdev <zanyxdev@gmail.com>
2019-01-15 16:16:03 +0300

    create debug cpu widget

--- src/cpuboxwidget.h
+++ src/cpuboxwidget.h
@@ -2,17 +2,14 @@
 #define CPUBOXWIDGET_H
 
 #include <QWidget>
-#include <QGroupBox>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QGridLayout>
-#include <QFormLayout>
 #include <QSpacerItem>
 #include <QLabel>
-#include <QFrame>
 #include <QTextEdit>
 #include <QLineEdit>
-#include <QTableView>
+#include <QListWidget>
 #include "mydefs.h"
 
 
@@ -20,29 +17,49 @@ class CPUBoxWidget : public QWidget
 {
     Q_OBJECT
 public:    
-    explicit CPUBoxWidget( QWidget *parent = Q_NULLPTR);    
+    explicit CPUBoxWidget( QWidget *parent = Q_NULLPTR);
+    void setModel(QAbstractListModel *m_model);
 
 public slots:
     void registerIChanged( quint16 value );
     void pointerCodeChanged( quint16 value);
     void delayTimerChanged( quint8 value );
     void soundTimerChanged( quint8 value );
-    void memoryCellChanged( quint16 value_0, quint16 value_1, quint16 value_2 );
+//    void memoryCellChanged( quint16 value_0, quint16 value_1, quint16 value_2 );
+    void registerValueChanged(quint8 m_reg ,quint8 value );
 
 private:    
-    QLineEdit* ILED;
-    QLineEdit* MemoryCellLED_0;
-    QLineEdit* MemoryCellLED_1;
-    QLineEdit* MemoryCellLED_2;
+    QLineEdit *ILED;
+//    QLineEdit *MemoryCellLED_0;
+//    QLineEdit *MemoryCellLED_1;
+//    QLineEdit *MemoryCellLED_2;
 
-    QLineEdit* PCLED;
-    QLineEdit* delayTimerLED;
-    QLineEdit* soundTimerLED;
-    QTextEdit* stackTEdit;        
+    QLineEdit *PCLED;
+    QLineEdit *delayTimerLED;
+    QLineEdit *soundTimerLED;
+
+    QLineEdit *v0LED;
+    QLineEdit *v1LED;
+    QLineEdit *v2LED;
+    QLineEdit *v3LED;
+    QLineEdit *v4LED;
+    QLineEdit *v5LED;
+    QLineEdit *v6LED;
+    QLineEdit *v7LED;
+
+    QLineEdit *v8LED;
+    QLineEdit *v9LED;
+    QLineEdit *vALED;
+    QLineEdit *vBLED;
+    QLineEdit *vCLED;
+    QLineEdit *vDLED;
+    QLineEdit *vELED;
+    QLineEdit *vFLED;
+
+//    QTextEdit *stackTEdit;
+    QListWidget *callStack;
 
     QVector<quint16> m_stack;     // deep 16 levels;
-
-    QTableView *registerTableView;
 };
 
 #endif // CPUBOXWIDGET_H
