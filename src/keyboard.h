6e965a31083c28afab5df64893cb6e696ed74a23
zanyxdev <zanyxdev@gmail.com>
2019-01-22 15:22:50 +0300

    use QmutexLocker for add and remove event keyUp keyDown form key events array

--- src/keyboard.h
+++ src/keyboard.h
@@ -2,16 +2,28 @@
 #define KEYBOARD_H
 
 #include <QObject>
+#include <QSet>
+#include <QMutex>
+#include <QMutexLocker>
+
+#include "mydefs.h"
 
 class Keyboard : public QObject
 {
     Q_OBJECT
 public:
     explicit Keyboard(QObject *parent = nullptr);
-
+    bool isPressed( quint8 key );
+    bool isNotPressed( quint8 key );
+    quint8 anyKeyPressed();
 signals:
 
 public slots:
+    void changeKeyState(quint8 key, bool state);
+
+private:
+    QSet<quint8> m_keys;
+    QMutex mutex;
 };
 
 #endif // KEYBOARD_H
